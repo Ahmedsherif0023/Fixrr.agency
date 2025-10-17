1c8895ebdfa14a358d299edff76f73081515311d
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-10-17 22:20:21 +0300

    making the main and the about sections

--- app/components/Silk.tsx
+++ app/components/Silk.tsx
@@ -1,15 +1,15 @@
 "use client";
 /* eslint-disable react/no-unknown-property */
-import React, { forwardRef, useMemo, useRef, useLayoutEffect } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import type { RootState } from '@react-three/fiber';
-import { Color, Mesh, ShaderMaterial } from 'three';
-import type { IUniform } from 'three';
+import React, { forwardRef, useMemo, useRef, useLayoutEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import type { RootState } from "@react-three/fiber";
+import { Color, Mesh, ShaderMaterial } from "three";
+import type { IUniform } from "three";
 
 type NormalizedRGB = [number, number, number];
 
 const hexToNormalizedRGB = (hex: string): NormalizedRGB => {
-  const clean = hex.replace('#', '');
+  const clean = hex.replace("#", "");
   const r = parseInt(clean.slice(0, 2), 16) / 255;
   const g = parseInt(clean.slice(2, 4), 16) / 255;
   const b = parseInt(clean.slice(4, 6), 16) / 255;
@@ -91,7 +91,10 @@ interface SilkPlaneProps {
   uniforms: SilkUniforms;
 }
 
-const SilkPlane = forwardRef<Mesh, SilkPlaneProps>(function SilkPlane({ uniforms }, ref) {
+const SilkPlane = forwardRef<Mesh, SilkPlaneProps>(function SilkPlane(
+  { uniforms },
+  ref
+) {
   const { viewport } = useThree();
 
   useLayoutEffect(() => {
@@ -114,11 +117,15 @@ const SilkPlane = forwardRef<Mesh, SilkPlaneProps>(function SilkPlane({ uniforms
   return (
     <mesh ref={ref}>
       <planeGeometry args={[1, 1, 1, 1]} />
-      <shaderMaterial uniforms={uniforms} vertexShader={vertexShader} fragmentShader={fragmentShader} />
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
     </mesh>
   );
 });
-SilkPlane.displayName = 'SilkPlane';
+SilkPlane.displayName = "SilkPlane";
 
 export interface SilkProps {
   speed?: number;
@@ -128,7 +135,13 @@ export interface SilkProps {
   rotation?: number;
 }
 
-const Silk: React.FC<SilkProps> = ({ speed = 5, scale = 1, color = '#7B7481', noiseIntensity = 1.5, rotation = 0 }) => {
+const Silk: React.FC<SilkProps> = ({
+  speed = 5,
+  scale = 1,
+  color = "#7B7481",
+  noiseIntensity = 1.5,
+  rotation = 0,
+}) => {
   const meshRef = useRef<Mesh>(null);
 
   const uniforms = useMemo<SilkUniforms>(
@@ -138,15 +151,27 @@ const Silk: React.FC<SilkProps> = ({ speed = 5, scale = 1, color = '#7B7481', no
       uNoiseIntensity: { value: noiseIntensity },
       uColor: { value: new Color(...hexToNormalizedRGB(color)) },
       uRotation: { value: rotation },
-      uTime: { value: 0 }
+      uTime: { value: 0 },
     }),
     [speed, scale, noiseIntensity, color, rotation]
   );
 
   return (
-    <Canvas dpr={[1, 2]} frameloop="always">
-      <SilkPlane ref={meshRef} uniforms={uniforms} />
-    </Canvas>
+    <div className="w-full h-full fixed inset-0 -z-10">
+      <Canvas
+        dpr={[1, 2]}
+        frameloop="always"
+        style={{
+          width: "100%",
+          height: "100vh",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      >
+        <SilkPlane ref={meshRef} uniforms={uniforms} />
+      </Canvas>
+    </div>
   );
 };
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,16 @@ import type { Route } from "./+types/home";
 import React from "react";
 import Silk from "../components/Silk"; // adjust the path based on where you put the file
 import { SiOpenjsfoundation } from "react-icons/si";
+import {
+  IoLogoJavascript,
+  IoLogoReact,
+  IoLogoNodejs,
+  IoLogoVercel,
+} from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandThreejs } from "react-icons/tb";
+import me from "../../public/imagee.webp";
+import { FaRegArrowAltCircleDown } from "react-icons/fa";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +22,7 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full h-screen scroll-smooth">
       {/* Silk background */}
       <div className="absolute inset-0 -z-10">
         <Silk
@@ -23,60 +33,172 @@ export default function Home() {
           rotation={0.2}
         />
       </div>
-      <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-1/2 bg-white/10 backdrop-blur-md text-white flex justify-between items-center py-4 px-6 rounded-full shadow-lg">
+
+      <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-1/2 bg-white/10 z-100 backdrop-blur-md text-white flex justify-between items-center py-4 px-6 rounded-full shadow-lg">
         <div className="flex flex-row items-center justify-center gap-5">
-          <img
-            src="./favicon.ico"
-            alt="FIXRR Logo"
-            className="w-10"
-          ></img>
+          <img src="./favicon.ico" alt="FIXRR Logo" className="w-10"></img>
           <h1 className="text-xl font-bold">Fixrr</h1>
         </div>
         <ul className="flex gap-6">
           <li>
-            <a href="#" className="font-bold">
+            <a href="#home-section" className="font-bold">
               Home
             </a>
           </li>
           <li>
-            <a href="#" className="font-bold">
+            <a href="#about-section" className="font-bold">
               About
             </a>
           </li>
-           <li>
+          <li>
             <a href="#" className="font-bold">
               Portfolio
             </a>
           </li>
           <li>
             <a href="#" className="font-bold">
-              Contact info
+              Contact
             </a>
           </li>
         </ul>
       </nav>
       {/* Your content */}
 
+      <div
+        className="flex items-center justify-center h-full flex-col gap-10 scroll-smooth"
+        id="home-section"
+      >
+        <div className="about-founder bg-white/10 backdrop-blur-md text-white flex justify-center items-center py-4 px-6 rounded-full shadow-lg font-bold gap-2">
+          {" "}
+          <SiOpenjsfoundation /> Crafting Unique Brand Identities
+        </div>
 
-      <div className="flex items-center justify-center h-full flex-col">
-        <a href="#" className="about-founder bg-white/10 backdrop-blur-md text-white flex justify-center items-center py-4 px-6 rounded-full shadow-lg font-bold gap-2"> <SiOpenjsfoundation /> About Us</a>
-
-        <h1 className="text-6xl text-white font-bold">Welcome to Fixrr</h1>
-        <h3 className="text-3xl text-white font-bold">
-          We Fix your digital presence
+        <h1 className="text-[110px] text-white font-[400] w-260 text-center justify-center leading-[100px]">
+          Branding that you need Indeed
+        </h1>
+        <h3 className="text-xl text-white/75 font-medium w-200 text-center justify-center mt-10 ">
+          Elevate your brand with custom identity and package design. Showcase
+          your story through bold visuals and strategic design solutions
         </h3>
-       <div className="flex flex-row gap-5 mt-10">
-        <a href="#" className="get-started bg-white text-black flex justify-center items-center py-4 px-6 rounded-full shadow-lg font-bold">Get Started</a>
-        <a href="#" className="learn-more bg-white/10 backdrop-blur-md text-white flex justify-center items-center py-4 px-6 rounded-full shadow-lg font-bold">Learn More</a>
+        <div className="flex flex-row gap-5 mt-10">
+          <a
+            href="#"
+            className="get-started bg-white text-black flex justify-center items-center py-3 px-4 rounded-[12px] shadow-lg font-bold"
+          >
+            Get Started Now
+          </a>
+          <a
+            href="#"
+            className="learn-more bg-white/10 backdrop-blur-md text-white flex justify-center items-center py-3 px-4 rounded-[12px] shadow-lg font-bold"
+          >
+            See Projects
+          </a>
+        </div>
+      </div>
+      <hr style={{ borderColor: "grey", borderWidth: "1px" }} />
+      <div className="relative w-full overflow-hidden py-3 mt-10 bg-white/10">
+        <ul className="flex animate-marquee whitespace-nowrap">
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoJavascript className="text-2xl" /> Javascript
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoReact className="text-2xl" /> React.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <RiTailwindCssFill className="text-2xl" /> Tailwindcss
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoNodejs className="text-2xl" /> Node.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <TbBrandThreejs className="text-2xl" /> Three.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoVercel className="text-2xl" /> Vercel
+          </li>
 
-       </div>
-
+          {/* duplicate to create infinite loop */}
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoJavascript className="text-2xl" /> Javascript
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoReact className="text-2xl" /> React.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <RiTailwindCssFill className="text-2xl" /> Tailwindcss
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoNodejs className="text-2xl" /> Node.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <TbBrandThreejs className="text-2xl" /> Three.Js
+          </li>
+          <li className="text-white font-medium flex items-center gap-3 mx-10">
+            <IoLogoVercel className="text-2xl" /> Vercel
+          </li>
+        </ul>
       </div>
 
+      <div
+        className="flex items-center justify-center h-full flex-row w-[90%] m-auto scroll-smooth"
+        id="about-section"
+      >
+        <div>
+          <div className="bg-white/10 z-100 backdrop-blur-md text-white w-[70%] p-15 rounded-xl">
+            <h1 className="font-bold text-4xl">Meet The founder</h1>
+            <p className="text-medium text-2xl text-white/85">
+              I'm Ahmed Sherif, a passionate software engineer based in EGYPT. I
+              specialize in crafting bold visual identities and great user
+              experience, also passionate about making a wonderful brand
+              identity and creative strategies to elevate brands.
+              <hr className="mt-10" />
+              <div className="flex flex-col gap-4 mt-10 font-semibold">
+                {/* First row: 2 items (each takes 1/2 width) */}
+                <ul className="grid grid-cols-2 gap-4">
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Front-end
+                  </li>
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Brand identity
+                  </li>
+                </ul>
 
-
-
-
+                {/* Rest of the rows: 3 items each */}
+                <ul className="grid grid-cols-3 gap-4">
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    UI/UX design
+                  </li>
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Branding
+                  </li>
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Figma
+                  </li>
+                </ul>
+                <ul className="grid grid-cols-2 gap-4">
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Illustrator
+                  </li>
+                  <li className="bg-white/20 backdrop-blur-md text-white rounded-xl p-2 text-center">
+                    Framer
+                  </li>
+                </ul>
+              </div>
+            </p>
+          </div>
+        </div>
+        <div>
+          <img
+            src={me}
+            alt="my image"
+            className="bg-white/20 w-300 rounded-2xl p-10 pb-0"
+          />
+        </div>
+      </div>
+      <hr style={{ borderColor: "grey", borderWidth: "1px" }} />
+      <div className="bg-white/80 backdrop-blur-md text-black rounded-xl p-1 text-center mt-[-18px] w-50 justify-center items-center align-center flex m-auto font-bold gap-1 cursor-pointer">
+        Recent Works <FaRegArrowAltCircleDown />
+      </div>
     </div>
   );
 }
