e8224bf488abdb1cb79da09dc704ea6e05fdd4e7
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-10-31 07:18:49 +0200

    finishing the first part of the website

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,62 @@
-import Image from "next/image";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex min-h-screen items-center justify-between  w-full">
+    <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 flex flex-row items-center justify-between gap-15 w-[80%] my-auto">
+     <div className="logo flex flex-row gap-5 items-center justify-center">
+       <img
+         src="/icon.webp"
+         alt="FIXRR agency logo"
+         width={50}
+         height={50}
+         priority
+       />
+       <h1 className="text-4xl font-semibold">FIXRR</h1>
+     </div>
+      <ul className="flex flex-row gap-7 text-lg ">
+        <li className=" cursor-pointer">Home</li>
+        <li className=" cursor-pointer">About Us</li>
+        <li className=" cursor-pointer">Projects</li>
+        <li className=" cursor-pointer">Services</li>
+        <li className=" cursor-pointer">Testimonial</li>
+      </ul>
+      <button className="border-2 border-white rounded-full px-6 py-2 flex flex-row items-center justify-center gap-3 cursor-pointer hover:bg-white hover:text-black transition duration-300">
+        Book a Free call <MdOutlineArrowRightAlt />
+      </button>
+    </nav> 
+  
+  <div className="main text-center justify-center items-center text-white relative w-[80%]">
+<img
+      src="https://cdn.prod.website-files.com/68fbf3ba4c59bf6b6664b8c9/68fbf3bb4c59bf6b6664b964_spring.png"
+      alt="spring"
+      fill
+      className="absolute inset-0 image-default -z-100 -top-20 left-80"
+      quality={100}
+      priority
+      width={700}
+    />
+
+    <div className="relative z-10 text-white w-[80%] text-left justify-start  left-20">
+      <h1 className="text-7xl font-semibold heading max-w-2xl text-left relative -top-20 left-20">
+        Emotion-Driven Design: Building Brand Loyalty
+      </h1>
+    </div>
+  </div>
+  <div className="static justify-end items-end text-white  w-full h-full">
+
+    <div className=" z-10 text-white w-[80%] text-left justify-end align-bottom ">
+     <div className="absolute text-base bottom-5 right-10 gap-7">
+       <h1 className="font-semibold heading  text-left max-w-sm ">
+         Passionate Egyptian  Team: Crafting Digital Solutions to Meet Your Needs. Empower Your Brand with Innovation.
+       </h1>
+       <button className="border-2 border-white rounded-full  px-6 py-2 flex flex-row items-center justify-center gap-3 cursor-pointer hover:bg-white hover:text-black transition duration-300 mt-5">
+         Book a Free call <MdOutlineArrowRightAlt />
+       </button>
+     </div>
+    </div>
+  </div>
+
     </div>
   );
 }
